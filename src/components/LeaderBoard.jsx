9d0b700cffb3bf1a3c61de89a839dbbb1507fbbf
Ashley Cao <ashleycao12@gmail.com>
2022-12-30 13:41:58 +0700

    make leaderboard multi page

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -3,18 +3,43 @@ import { getTopScores } from "../index"
 import {Link, useParams} from 'react-router-dom'
 
 function LeaderBoard() {
+  const itemNumPerPage = 5
+  // const activePageStyle = {backgroundColor: "gray"}
+
   const { game } = useParams()
   const [scores, setScores] = useState([])
+  const [pageIndex, setPageIndex] = useState(0)
+
+  const getScorePage = () => {
+    const startingIndex = itemNumPerPage*pageIndex
+    const page = scores.slice(startingIndex, startingIndex + itemNumPerPage)
+    return page
+  }
+
+  const getPageList = () => {
+    const pageNum = Math.round(scores.length / itemNumPerPage)
+    const result = []
+    for (let i = 0; i < pageNum; i++) {
+      result.push(i+1)
+    }
+    return result
+  }
+
   useEffect(() => {
     const showScore = async () => {
       const data = await getTopScores(game)
-      setScores(data)
+      const scoreArr = data.map((score, index) => {
+        return {...score, rank: index + 1}
+      })
+      setScores(scoreArr)
     }
     showScore()
   },[])
   return (
     <div className=" bg-white w-4/6 mx-auto mt-24 text-center p-5">
       <h2>Leader Board</h2>
+
+      {/****  Table ******/}
       <table className=" bg-purple-100 mx-auto w-full">
         <tbody>    
         <tr className=" bg-purple-800">
@@ -22,15 +47,24 @@ function LeaderBoard() {
           <th>Name</th>
           <th>Score</th>
         </tr>
-      {scores?.map((score,index) =>
+      {/* {scores?.map((score,index) => */}
+      {getScorePage().map((score,index) =>
         <tr key={score.id} className=" bg-purple-400 pt-10">
-          <td>{index+1}</td>
+          <td>{score.rank}</td>
           <td>{score.name}</td>
           <td>{score.score}</td>
         </tr>
         )} 
         </tbody>
       </table>
+      
+      {/* Page number */}
+      <p> 
+        {getPageList().map(page =>
+          <button onClick={() => { setPageIndex(page - 1) }} key={page} className=" mx-2"> {page} </button>
+        )}
+      </p>
+
       <button className=" mt-5 border"><Link to={"/" + game }>Play again</Link></button>
     </div>
   )
