53b12dae4c4a0df02e6223bce5ba42a2d681fa6a
Ashley Cao <ashleycao12@gmail.com>
2022-12-30 07:47:54 +0700

    style submit button

--- src/components/SaveSnakeScore.jsx
+++ src/components/SaveSnakeScore.jsx
@@ -21,7 +21,7 @@ function SaveSnakeScore({score}) {
     <form>
       <label> Your name: </label>
       <input type="text" onChange={handleChange}/>
-      <button onClick={handleSubmit}> Submit </button>
+      <button onClick={handleSubmit} className = " bg-slate-200 px-2 py-1 ml-2 hover:bg-slate-300"> Submit </button>
     </form>
   )
 }
