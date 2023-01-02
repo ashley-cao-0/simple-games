133056c02365e084f9e799d264bc85322df871f7
Ashley Cao <ashleycao12@gmail.com>
2023-01-02 14:26:29 +0700

    styling

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -3,9 +3,9 @@ import { getTopScores } from "../index"
 import {Link, useParams, useSearchParams} from 'react-router-dom'
 
 function LeaderBoard() {
-  const itemPerPage = 5
-  const activePageStyle = { backgroundColor: "pink" }
-  const userScoreStyle = { backgroundColor: "pink" }
+  const itemPerPage = 10
+  const activePageStyle = { backgroundColor: "LightPink" }
+  const userScoreStyle = { backgroundColor: "#404040", color: "white"}
 
   const { game } = useParams()
   const [searchParams] = useSearchParams()
@@ -70,47 +70,49 @@ function LeaderBoard() {
   }, [])
 
   return (
-    <div className=" bg-white w-4/6 mx-auto mt-24 text-center p-5">
-      <h2>
-        {game === 'snake' ? <span> Snake </span> : <span> Math Speed </span>}
-        Leader Board
-      </h2>
+    <div className=" flex justify-center">
+      <div className=" bg-white inline-block mt-24 text-center py-7 px-20 border">
+        <h2 className=" text-2xl text-white mb-2 py-1 bg-cyan-900 border rounded-md">
+          {game === 'snake' ? <span> Snake </span> : <span> Math Speed </span>}
+          Leader Board
+        </h2>
 
-      {/****  Table ******/}
-      <table className=" mx-auto w-full">
-        <tbody>    
-        <tr className=" bg-purple-800">
-          <th>Rank</th>
-          <th>Name</th>
-          <th>Score</th>
-        </tr>
-      {/* {scores?.map((score,index) => */}
-      {getScorePage().map(score =>
-        <tr key={score.id} className=" bg-purple-400 pt-10" style = {score.style}>
-          <td>{score.rank}</td>
-          <td>{score.name}</td>
-          <td>{score.score}</td>
-        </tr>
-        )} 
-        </tbody>
-      </table>
-      
-      {/* Page numbers */}
-      <p className=" mt-4">
-        {/* previous page button*/}
-        <button onClick={ toPreviousPage } className = " mr-2 px-1 rounded bg-slate-200"> {'<'} </button>
+        {/****  Table ******/}
+        <table className=" mx-auto w-96">
+          <tbody>    
+          <tr className=" bg-sky-900 text-lg text-white">
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        {/* {scores?.map((score,index) => */}
+        {getScorePage().map(score =>
+          <tr key={score.id} className=" bg-sky-200 pt-10 " style = {score.style} >
+            <td>{score.rank}</td>
+            <td>{score.name}</td>
+            <td>{score.score}</td>
+          </tr>
+          )} 
+          </tbody>
+        </table>
+        
+        {/* Page numbers */}
+        <p className=" mt-4">
+          {/* previous page button*/}
+          <button onClick={ toPreviousPage } className = " mr-2 px-1 rounded bg-slate-200"> {'<'} </button>
 
-        {/* page list */}
-        {getPageList().map(page =>
-          <button onClick={() => {setPageIndex(page.pageNumber - 1)}} style = {page.style} className = " px-1 mx-1" key={page.pageNumber}> {page.pageNumber} </button>
-        )}
+          {/* page list */}
+          {getPageList().map(page =>
+            <button onClick={() => {setPageIndex(page.pageNumber - 1)}} style = {page.style} className = " px-1 mx-1" key={page.pageNumber}> {page.pageNumber} </button>
+          )}
 
-        {/* next page button */}
-        <button onClick={ toNextPage } className = " ml-2 px-1 rounded bg-slate-200"> {'>'} </button>
-      </p>
+          {/* next page button */}
+          <button onClick={ toNextPage } className = " ml-2 px-1 rounded bg-slate-200"> {'>'} </button>
+        </p>
 
-      <button className=" mt-5 border"><Link to={"/" + game }> Back to game </Link></button>
+        <button className=" mt-5 border border-gray-400 px-1"><Link to={"/" + game }> Back to game </Link></button>
     </div>
+  </div>
   )
 }
 
