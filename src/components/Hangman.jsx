476e4f5d9bbb1c8785fedc2f7b75c6b4d239f47c
Ashley Cao <ashleycao12@gmail.com>
2023-01-02 14:50:28 +0700

    fetch and render a random word

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getRandomWord } from "../apiClient";
 
 function Hangman() {
+  const [word, setWord] = useState()
+
+  useEffect(() => {
+    const getWord = async () => {
+      const randomWord = await getRandomWord()
+      setWord(randomWord)
+    }
+    getWord()
+  }, [])
+
   return (
     <div className=" mt-24 text-center ">
       <h1 className="text-5xl mb-10"> Hangman </h1>
-      
+      <p> { word } </p>
     </div>
   )
 }
