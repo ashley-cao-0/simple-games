e27275736d2c33c5e21644f9184ad21c9d596bfd
Ashley Cao <ashleycao12@gmail.com>
2023-01-04 23:02:15 +0700

    reorganise + responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,16 @@ import Card from "./Card";
 function Home() {
   const contentList = [
     {
+      route: '/snake',
+      description: 'Let\'s play snake!',
+      img: '/snake.png',
+    },
+    {
+      route: '/hangman',
+      description: 'Let\'s play hangman',
+      img: '/hangman.jpg',
+    },
+    {
       route: '/mathtest',
       description: 'Test your math Speed',
       img: '/numbers.jpg',
@@ -14,24 +24,15 @@ function Home() {
       img: '/typing.jpeg',
     },
     {
-      route: '/snake',
-      description: 'Let\'s play snake!',
-      img: '/snake.png',
-    },
-    {
       route: '/memory',
       description: 'Test your working memory',
       img: 'memory.jpg',
     },
-    {
-      route: '/hangman',
-      description: 'Let\'s play hangman',
-      img: '/hangman.jpg',
-    },
+  
   ]
   return (
     <div className=" flex justify-center items-center mt-24"> 
-      <div className=" mt-10 grid gap-4 grid-cols-2">
+      <div className=" mt-10 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {contentList.map(content => <Card content = {content} key={content.route} />)}
       </div>
     </div>
