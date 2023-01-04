f8549c380cbbc0450902b8995381d4eafc2d8fe0
Ashley Cao <ashleycao12@gmail.com>
2023-01-04 14:02:59 +0700

    render dying face on losing

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getRandomWord } from "../apiClient";
 
 function Hangman() {
-  const clickedStyle = { backgroundColor: "grey" }
+  const clickedStyle = { backgroundColor: "grey"}
   
   const [alphabet, setAlphabet] = useState([])
   const [givenLetters, setLetters] = useState([])
@@ -28,12 +28,12 @@ function Hangman() {
     return result
   }
 
-  const handleClick = (e, guess, index) => {
-    e.target.disabled = true
+  const handleClick = (guess, index) => {
 
     //gray out clicked button
     const newAlphabet = [...alphabet]
     newAlphabet[index].style = clickedStyle
+    newAlphabet[index].disabled = true
     setAlphabet(newAlphabet)
 
     //check guess
@@ -75,18 +75,22 @@ function Hangman() {
     <div className=" mt-24 text-center flex justify-center flex-col h-screen">
       <h1 className="text-5xl mb-10"> Hangman </h1>
 
+      {/* hangman drawing */}
       <div className=" grow flex max-w-6xl mx-auto">
         <div className=" w-96 relative basis-2/3">
           {getStrokes().map(strokeNum =>
             <img src={`/hangman/${ strokeNum }.png`} alt={"stroke " + strokeNum} className=" absolute max-w-full max-h-full" key={ strokeNum} />
           )}
+
+          {/* render face when lost */}
+          {lost() && <img src="/hangman/11.png" alt="dying face" className=" absolute max-w-full max-h-full" />}
         </div>
 
         <div>  
           <div className=" flex justify-center mt-16 mb-24">
             <div className="inline-block max-w-5xl">
               {alphabet.map((item, index) =>
-                <button className=" bg-orange-100 w-14 m-1" onClick={(e) => {handleClick(e, item.char, index)}} style= {item.style} key={item.char} > {item.char} </button>
+                <button className=" bg-orange-100 w-14 m-1" onClick={() => {handleClick(item.char, index)}} style= {item.style} disabled= {item.disabled} key={item.char} > {item.char} </button>
                 )}
             </div>
           </div>
@@ -94,13 +98,13 @@ function Hangman() {
           {/* <p> Wrong guesses: { wrongGuesses } </p> */}
           <p className=" h-5"> {givenLetters} </p>
           {/* <p className=" text-3xl"> {progress} </p> */}
-          <p>
+          <h2>
             {progress.map(char => <span className=" mr-1 text-5xl">{char}</span>)}
-          </p>
+          </h2>
 
 
-          {won() && <p> Congrats </p>}
-          {lost() && <p> You lose </p>}
+          {won() && <h2 className=" text-3xl mt-10"> Congrats </h2>}
+          {lost() && <h2 className=" text-3xl mt-10"> You lose </h2>}
         </div>
       </div>
     </div>
