ce84ab4390f4b7c58ad47dc5d945e88072fa020c
Ashley Cao <ashleycao12@gmail.com>
2023-01-11 14:13:42 +0700

    use saveScore instead of finshScreen

--- src/components/MathTest.jsx
+++ src/components/MathTest.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from 'react-router-dom'
-import FinishScreen from "./FinishScreen";
+import SaveScore from "./SaveScore";
 
 function MathTest() {
   const randomNum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
@@ -61,30 +61,31 @@ function MathTest() {
   return (
     <>
       <Link to="/leaderboard/mathtest" className='absolute z-50 py-3 px-5 m-4 ml-32 text-2xl bg-slate-300 hover:bg-slate-400 duration-150'> Leader board </Link>
-      {
-        !timesUp ?
-        // timesUp ? 
-        <div className=" text-center">
-          <h2 className="mt-24 text-xl">Correct answers: {count}</h2>
-          <div className=" w-96 mx-auto mt-5">
-            <div className={'bg-red-500 h-1 w-full ' + timerAnimation}></div>
-            <div className=" bg-white p-4 rounded-md h-36 flex items-center justify-center">
-              <h2 className=" text-4xl">{num1} + {num2}</h2>
-            </div>
-          </div>
-          
-          <form onSubmit={handleSubmit} className={`w-96 mx-auto mt-5 border`}>
-            <input type="text" name="answer" value={input} ref={ref} onChange={handleChange} autoComplete="off" className={`w-full focus:outline-0 border border-gray-400 ${ inputAnimation }`} onAnimationEnd={resetInputAnimation} />
-          </form>
-          
-          {!started &&
-            <p className="w-96 mx-auto mt-5"> You have 30 seconds. The timer will start after your first answer. </p>
-          }
 
+      <div className=" text-center">
+        <h2 className="mt-24 text-xl">Correct answers: {count}</h2>
+        <div className=" w-96 mx-auto mt-5">
+          <div className={'bg-red-500 h-1 w-full ' + timerAnimation}></div>
+          <div className=" bg-white p-4 rounded-md h-36 flex items-center justify-center">
+          {!timesUp ?
+            <h2 className=" text-4xl">{num1} + {num2}</h2>
+            :
+            <h2 className=" text-4xl">Time's up</h2>
+          }
+          </div>
         </div>
-        :
-        <FinishScreen score={count} game="mathtest" />
-      }
+        
+        <form onSubmit={handleSubmit} className={`w-96 mx-auto mt-5 border`}>
+        <input type="text" name="answer" value={input} ref={ref} onChange={handleChange} autoComplete="off" className={`w-full focus:outline-0 border border-gray-400 ${ inputAnimation }`}
+          onAnimationEnd={resetInputAnimation} disabled={timesUp} />
+        </form>
+        
+        {!started &&
+          <p className="w-96 mx-auto mt-5"> You have 30 seconds. The timer will start after your first answer. </p>
+        }
+        
+        {timesUp && <SaveScore game='mathtest' score={count} />}
+      </div>       
     </>  
     
   )
