ed6ba9be37cad31a138428043d5f7846658d5eaf
Ashley Cao <ashleycao12@gmail.com>
2023-01-11 15:25:02 +0700

    change images and styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ import StopTheGhost from './StopTheGhost'
 
 function App() {
   return (
-    <div className='bg-slate-100 absolute min-h-full w-full font-mono'>
+    <div className='bg-slate-50 absolute min-h-full w-full font-mono'>
       <Link to="/" className='absolute z-50 py-3 px-5 m-4 text-2xl bg-slate-300 hover:bg-slate-400 duration-150'>Home</Link>
     <Routes>
       <Route path='/' element={<Home/>}/>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,12 @@ function Home() {
     {
       route: '/snake',
       description: 'Let\'s play snake!',
-      img: '/snake.png',
+      img: '/snake.jpg',
     },
     {
       route: '/mathtest',
       description: 'Test your math Speed',
-      img: '/numbers.jpg',
+      img: '/math.jpg',
     },
     {
       route: '/hangman',
@@ -21,12 +21,12 @@ function Home() {
     {
       route: '/typingtest',
       description: 'Test your typing speed',
-      img: '/typing.jpeg',
+      img: '/typing.jpg',
     },
     {
       route: '/memory',
       description: 'Test your working memory',
-      img: 'memory.jpg',
+      img: 'memory.png',
     },
     {
       route: '/stoptheghost',
@@ -37,7 +37,7 @@ function Home() {
   ]
   return (
     <div className=" flex justify-center items-center mt-24 mb-16"> 
-      <div className=" mt-10 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className=" mt-10 grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {contentList.map(content => <Card content = {content} key={content.route} />)}
       </div>
     </div>
