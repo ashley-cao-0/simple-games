b3a7e7eea8f2a815b8bc2fd73695df1948e9b19c
Ashley Cao <ashleycao12@gmail.com>
2023-01-11 15:35:56 +0700

    minor change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({content}) => {
     <Link to= {route}>
       <div className=" flex flex-col w-80 h-60 hover:shadow-2xl transition-all duration-150 shadow-md shadow-slate-500 bg-white rounded-md overflow-hidden">
         <h1 className=" text-xl p-3"> {description} </h1>
-        <img src= {img} className=" flex-grow object-cover"/>
+        <img src= {img} className=" flex-grow"/>
       </div>
      </Link>
    );
