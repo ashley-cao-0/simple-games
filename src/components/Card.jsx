a7b583c85d6d0e1b1803c0d7c3ad24d70ccdf054
Ashley Cao <ashleycao12@gmail.com>
2023-01-11 16:14:56 +0700

    styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({content}) => {
   const {route, description, img} = content
   return ( 
     <Link to= {route}>
-      <div className=" flex flex-col w-80 h-60 hover:shadow-2xl transition-all duration-150 shadow-md shadow-slate-500 bg-white rounded-md overflow-hidden">
+      <div className=" flex flex-col w-80 h-60 hover:shadow-2xl transition-all duration-150 shadow-md shadow-slate-500 border border-slate-400 bg-white rounded-md overflow-hidden">
         <h1 className=" text-xl p-3"> {description} </h1>
         <img src= {img} className=" flex-grow"/>
       </div>
