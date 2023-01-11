b578425aeaa95b3107a8fb776fada775faf7693c
Ashley Cao <ashleycao12@gmail.com>
2023-01-11 14:33:34 +0700

    play again feature

--- src/components/MathTest.jsx
+++ src/components/MathTest.jsx
@@ -17,7 +17,16 @@ function MathTest() {
   
   const ref = useRef()
 
-  const refreshQuestion = () =>{
+  const resetGame = () => {
+    refreshQuestion()
+    setCount(0)
+    setInput('')
+    setStarted(false)
+    setTimesUp(false)
+    setTimerAnimation('')
+  }
+
+  const refreshQuestion = () => {
     setNum1(randomNum(10,99))
     setNum2(randomNum(10,99))
   }
@@ -63,14 +72,14 @@ function MathTest() {
       <Link to="/leaderboard/mathtest" className='absolute z-50 py-3 px-5 m-4 ml-32 text-2xl bg-slate-300 hover:bg-slate-400 duration-150'> Leader board </Link>
 
       <div className=" text-center">
-        <h2 className="mt-24 text-xl">Correct answers: {count}</h2>
+        <h2 className="mt-24 text-xl">Score: {count}</h2>
         <div className=" w-96 mx-auto mt-5">
           <div className={'bg-red-500 h-1 w-full ' + timerAnimation}></div>
           <div className=" bg-white p-4 rounded-md h-36 flex items-center justify-center">
           {!timesUp ?
             <h2 className=" text-4xl">{num1} + {num2}</h2>
             :
-            <h2 className=" text-4xl">Time's up</h2>
+            <h2 className=" text-4xl">Time's up!</h2>
           }
           </div>
         </div>
@@ -84,7 +93,12 @@ function MathTest() {
           <p className="w-96 mx-auto mt-5"> You have 30 seconds. The timer will start after your first answer. </p>
         }
         
-        {timesUp && <SaveScore game='mathtest' score={count} />}
+        {timesUp &&
+          <div className=" flex justify-center">
+          <SaveScore game='mathtest' score={count} />
+          <button className=" mt-10 mx-2 text-xl py-2 px-4 border border-slate-500 bg-slate-300" onClick={resetGame}>Play again</button>
+          </div>
+        }
       </div>       
     </>  
     
