876e28a754d5e03c496999723519a9d5c0dfae62
Ashley Cao <ashleycao12@gmail.com>
2023-01-12 20:15:14 +0700

    delete unused functions

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,24 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import request from 'superagent'
 // eslint-disable-next-line no-unused-vars
-const scoreUrl = '/api/v1/scores/'
-
-export async function saveScore(name, scoreId){
-  await request.patch(scoreUrl+scoreId).send({name})
-}
-
-export async function saveAnonymousScore(score){
-  const response = await request.post('/api/v1/scores').send({name:'anonymous', score})
-    const id = await response.body.id[0]
-  return id
-}
-
-export async function getTopScore(scoreNum){
-  const response = await request.get('/api/v1/scores/top/'+scoreNum)
-  const data = await response.body
-  console.log(data);
-  return data
-}
 
 export async function getRandomParagraphs(){
   const response = await request.get('http://metaphorpsum.com/paragraphs/3')
