32fde878bd31e3064a6b2cc793b23b5ce412a757
Ashley Cao <ashleycao12@gmail.com>
2023-01-24 00:18:08 +0700

    track onKeyDown

--- src/components/Sudoku.jsx
+++ src/components/Sudoku.jsx
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import { getSudoku } from "../apiClient";
 
 function Sudoku() {
+  const ref = useRef(null)
   const [board, setBoard] = useState([])
   const [selectedCell, setSelectedCell] = useState([0, 0])
   
@@ -13,7 +14,12 @@ function Sudoku() {
     }
   }
 
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+  }
+
   useEffect(() => {
+    ref.current.focus()
     const getNewSudoku = async () => {
       const newSudoku = await getSudoku()
       setBoard(newSudoku.value)
@@ -21,11 +27,11 @@ function Sudoku() {
     getNewSudoku()
   },[])
   return (
-    <div className=" mt-24">
-      <div className=" flex justify-center">
+    <div ref={ref} onKeyDown={handleKeyDown} tabIndex={-1} className=" min-h-full w-full absolute top-0">
+      <div className=" flex justify-center mt-24">
       {/* Empty board with border to hold digits */}
         {/* grid for 3 big blocks with heavy border*/}
-        <div className=" z-40 absolute grid grid-cols-3 border border-black">
+        <div className=" z-30 absolute grid grid-cols-3 border border-black">
           {Array(9).fill().map((item, i) => {
             return <div key={i} className=" border border-black w-24 h-24">  </div>
           }
@@ -46,7 +52,7 @@ function Sudoku() {
           {board.map((row, iRow)=>
             <div key={iRow} className="flex">
               {row.map((digit, iCol) =>
-                <div key={iCol} onClick = {() => setSelectedCell([iRow, iCol]) }  className="z-50 w-8 h-8 flex justify-center items-center bg-transparent cursor-default" >
+                <div key={iCol} onClick = {() => setSelectedCell([iRow, iCol]) }  className="z-40 w-8 h-8 flex justify-center items-center bg-transparent cursor-default" >
                   {digit !==0 && <p> {digit} </p>}
                 </div>
               
@@ -55,7 +61,7 @@ function Sudoku() {
           }
         </div>
       </div>
-    </div>
+      </div>
   )
 }
 
