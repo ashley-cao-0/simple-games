392c9d7048267c7bd666aa163774b56fc8eac6fa
Ashley Cao <ashleycao12@gmail.com>
2023-01-23 23:36:36 +0700

    add sodoku client api

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-unused-vars
+import { async } from '@firebase/util'
 import request from 'superagent'
 // eslint-disable-next-line no-unused-vars
 
@@ -12,4 +13,10 @@ export async function getRandomWord() {
   const response = await request.get('https://api.api-ninjas.com/v1/randomword').set('X-Api-Key', 'wWYBp+wEvygQbwasYSVhTg==ABoCohj4Tv8UiGOj')
   const word = response.body.word
   return word
+}
+
+export async function getSudoku() {
+  const response = await request.get('https://sudoku-api.vercel.app/api/dosuku')
+  const sudoku = response.body.newboard.grids[0]
+  return sudoku
 }
