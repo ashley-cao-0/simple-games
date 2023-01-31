9ff83686a84fc328f0bda072352d4aa3fcdb14dd
Ashley Cao <ashleycao12@gmail.com>
2023-01-31 22:04:00 +0700

    add notFound page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,23 +11,25 @@ import MemoryTest from './MemoryTest'
 import Hangman from './Hangman'
 import StopTheGhost from './StopTheGhost'
 import Sudoku from './Sudoku'
+import NotFound from './NotFound'
 
 function App() {
   return (
     <div className='bg-slate-50 absolute min-h-full w-full font-mono'>
       <Link to="/" className='absolute z-50 py-3 px-5 m-4 text-2xl bg-slate-300 hover:bg-slate-400 duration-150'>Home</Link>
       <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/mathtest' element={<MathTest/>}/>
-      <Route path='/leaderboard/:game' element={<LeaderBoard />}/>
-      <Route path='/typingtest' element={<TypingTest />}/>
-      <Route path='/game' element={<Game />}/>
-      <Route path='/snake' element={<Snake />}/>
-      <Route path='/memory' element={<MemoryTest />}/>
-      <Route path='/hangman' element={<Hangman />}/>
-      <Route path='/stoptheghost' element={<StopTheGhost />}/>
-      <Route path='/sudoku' element={<Sudoku />}/>
-    </Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/mathtest' element={<MathTest/>}/>
+        <Route path='/leaderboard/:game' element={<LeaderBoard />}/>
+        <Route path='/typingtest' element={<TypingTest />}/>
+        <Route path='/game' element={<Game />}/>
+        <Route path='/snake' element={<Snake />}/>
+        <Route path='/memory' element={<MemoryTest />}/>
+        <Route path='/hangman' element={<Hangman />}/>
+        <Route path='/stoptheghost' element={<StopTheGhost />}/>
+        <Route path='/sudoku' element={<Sudoku />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
     </div>
   )
 }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className=" mt-24 text-center text-zinc-600">
+      <h1 className=" text-9xl"> 404 </h1>
+      <h2 className=" text-6xl"> Page not found </h2>
+    </div>
+  )
+}
+
+export default NotFound
